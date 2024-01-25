bc6ab1023ef7deec084534022596c88a5c0c06af
Abinash Rai <abinashbantawa1905@gmail.com>
2024-01-25 22:50:44 +0545

    working on the navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <header>My-Commerce</header>
+    <div className="d-flex flex-column h-full"></div>
+      <header>Express-Cart</header>
       <main>Products</main>
       <footer>All rights reserved</footer>
     </>
---
+++ frontend/src/data.ts
@@ -0,0 +1,5 @@
+// export const sampleProducts: Products[] = [
+//     {
+//         name: 'Nike Slim shirt'
+//     }
+// ]
